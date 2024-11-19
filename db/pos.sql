f28ece09514d27f6c78c16355aada40c046bbdf4
LABKOM <LABKOM@D1MDOSEN>
2024-11-19 14:50:54 +0700

    update 1

--- db/pos.sql
+++ db/pos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 15, 2024 at 10:48 AM
+-- Generation Time: Nov 19, 2024 at 08:50 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,77 @@ SET time_zone = "+00:00";
 --
 -- Database: `pos`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `product_code` varchar(15) NOT NULL,
+  `product_name` varchar(60) NOT NULL,
+  `product_image` varchar(50) NOT NULL,
+  `product_category` int(11) NOT NULL,
+  `product_supplier` int(11) NOT NULL,
+  `product_price_s` double NOT NULL,
+  `product_price_b` double NOT NULL,
+  `product_stock` bigint(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `product_code`, `product_name`, `product_image`, `product_category`, `product_supplier`, `product_price_s`, `product_price_b`, `product_stock`) VALUES
+(4, 'P00001', 'Printer', 'C:\\Users\\LABKOM\\Pictures\\Driver-Mongo.PNG', 1, 2, 5000000, 4000000, 50),
+(5, 'P00002', 'Laptop Dell', 'C:\\Users\\LABKOM\\Pictures\\Driver-Mongo.PNG', 1, 2, 7500000, 5000000, 30),
+(6, 'P00003', 'dsfdsfdssdfdsf', 'fdsfdsfdsfdsfds', 1, 2, 555, 5555, 5),
+(7, 'P00004', 'AC Daikin', 'C:\\Users\\LABKOM\\Pictures\\Save-as-icon.png', 1, 2, 3000000, 2500000, 15),
+(8, 'P00005', 'Mesin Cuci 2 tabung', 'C:\\Users\\LABKOM\\Pictures\\Driver-Mongo.PNG', 1, 2, 6000000, 5000000, 50);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_category`
+--
+
+CREATE TABLE `product_category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product_category`
+--
+
+INSERT INTO `product_category` (`id`, `name`, `description`) VALUES
+(1, 'Elektronik', 'Elektronik'),
+(2, 'Fashion', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `id` int(11) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `phone` varchar(13) NOT NULL,
+  `address` varchar(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `supplier`
+--
+
+INSERT INTO `supplier` (`id`, `name`, `phone`, `address`) VALUES
+(1, 'AFFA COLLECTION', '08151222444', 'A'),
+(2, 'ElektronikQ', '081577744411', 'B'),
+(3, 'Sinar Jaya', '0816458545', 'Tegal');
 
 -- --------------------------------------------------------
 
@@ -41,11 +112,36 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `fullname`, `username`, `password`, `level`) VALUES
 (1, 'SEKAR QYNASIH INGGIL ARDINSYAH', 'sena', '1234', 'admin'),
-(2, 'AHMAT FAUZI', 'fauzi', '123', 'kasir');
+(2, 'AHMAT FAUZI', 'fauzi', '123', 'kasir'),
+(16, 'fahmi', 'fahmi', '1234', 'owner'),
+(17, 'fahmi', 'fahmi2', '1234', 'owner'),
+(18, 'fahmi', 'fahmi3', '1234', 'owner'),
+(19, 'sena2', 'sena2', '1234', 'kasir'),
+(20, 'A', 'A', 'A', 'admin');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_supplier` (`product_supplier`),
+  ADD KEY `product_category` (`product_category`);
+
+--
+-- Indexes for table `product_category`
+--
+ALTER TABLE `product_category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -58,10 +154,39 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `product_category`
+--
+ALTER TABLE `product_category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `supplier`
+--
+ALTER TABLE `supplier`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`product_supplier`) REFERENCES `supplier` (`id`),
+  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`product_category`) REFERENCES `product_category` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
